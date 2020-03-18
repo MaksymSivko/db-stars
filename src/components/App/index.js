707fd7d92c26c5c54af7134ce99961003ba89c81
Maksym Sivko <maksym.sivko@gmail.com>
2020-03-18 17:43:03 +0200

    refat

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,29 +2,20 @@ import React, { Component } from 'react';
 import { Header } from '../Header';
 import { ErrorBoundry } from '../ErrorBoundry';
 import { RandomPlanet } from '../RandomPlanet';
-import { Row } from '../Row';
+import { PeoplePage } from '../Pages/people-page';
+import { PlanetPage } from '../Pages/planet-page';
+import { StarshipPage } from '../Pages/starship-page';
 
 import { SwapiService } from '../../services/swapi-service';
 import { DummySwapiService } from '../../services/dummy-swapi-service';
 
 import { SwapiServiceProvider } from '../Swapi-service-context';
 
-import PersonDetails from '../sw-components/Person-details';
-import PlanetDetails from '../sw-components/Planet-details';
-import StarshipDetails from '../sw-components/Starship-details';
-
-import {
-  PersonList,
-  PlanetList,
-  StarshipList
-} from '../sw-components/ListItem';
-
 import './style.css';
 
 export class App extends Component {
   state = {
-    showRandomPlanet: true,
-    swapiService: new DummySwapiService()
+    swapiService: new SwapiService()
   };
 
   onServiceChange = () => {
@@ -38,33 +29,20 @@ export class App extends Component {
     });
   };
 
-  toggleRandomPlanet = () => {
-    this.setState(state => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      };
-    });
-  };
-
   render() {
-    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService}>
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
 
-            {planet}
+            <RandomPlanet />
 
-            <Row left={<PersonList />} right={<PersonDetails itemId={11} />} />
+            <PeoplePage />
 
-            <Row left={<PlanetList />} right={<PlanetDetails itemId={5} />} />
+            <PlanetPage />
 
-            <Row
-              left={<StarshipList />}
-              right={<StarshipDetails itemId={9} />}
-            />
+            <StarshipPage />
           </div>
         </SwapiServiceProvider>
       </ErrorBoundry>
--- src/components/HOC/with-data.js
+++ src/components/HOC/with-data.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import { ErrorIndicator } from '../ErrorIndicator';
 import { Spinner } from '../Spinner';
 
 export const withData = View => {
   return class extends Component {
     state = {
-      data: null
+      data: null,
+      loading: true,
+      error: false
     };
 
     componentDidMount() {
@@ -19,18 +21,36 @@ export const withData = View => {
     }
 
     updata() {
-      this.props.getData().then(data => {
-        this.setState({
-          data
-        });
+      this.setState({
+        loading: true,
+        error: false
       });
+
+      this.props
+        .getData()
+        .then(data => {
+          this.setState({
+            data,
+            loading: false
+          });
+        })
+        .catch(() => {
+          this.setState({
+            error: true,
+            loading: false
+          });
+        });
     }
 
     render() {
-      const { data } = this.state;
+      const { data, loading, error } = this.state;
 
-      if (!data) {
+      if (loading) {
         return <Spinner />;
+      }
+
+      if (error) {
+        return <ErrorIndicator />;
       }
 
       return <View {...this.props} data={data} />;
